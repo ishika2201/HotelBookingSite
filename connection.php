d780d893585f443325cb478929420e8fcbd142a0
ishika2201 <97585000+ishika2201@users.noreply.github.com>
2022-03-13 14:46:22 +0530

    Add files via upload

---
+++ connection.php
@@ -0,0 +1,14 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$database="bookmyhotel";
+$password = "";
+
+// Create connection
+$db = new mysqli($servername, $username, $password,$database);
+
+// Check connection
+if ($db->connect_error) {
+  die("Connection failed: " . $db->connect_error);
+}
+
